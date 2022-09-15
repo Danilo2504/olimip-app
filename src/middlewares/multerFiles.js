bd89330a5695c126195dfe47dff37d41627e6c40
LucianoBrumer <lucianobrumer5@gmail.com>
2022-09-15 17:09:47 -0300

    upload images

--- src/middlewares/multerFiles.js
+++ src/middlewares/multerFiles.js
@@ -4,12 +4,10 @@ const path = require('path')
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         if(file.fieldname == 'image') cb(null, 'public/uploads/images')
-        if(file.fieldname == 'datapack') cb(null, 'public/uploads/datapacks')
-        if(file.fieldname == 'resourcepack') cb(null, 'public/uploads/resourcepacks')
     },
     filename: (req, file, cb) => {
         // cb(null, `${Date.now()}-${file.originalname}`)
-        cb(null, `${req.body.title + path.extname(file.originalname)}`)
+        cb(null, `${req.body.name + path.extname(file.originalname)}`)
     }
 })
 
@@ -17,9 +15,5 @@ module.exports = multer({
     storage,
     limits: {fileSize: 1000000}
 }).fields([{
-    name: 'image', maxCount: 5
-},{
-    name: 'datapack', maxCount: 1
-},{
-    name: 'resourcepack', maxCount: 1
+    name: 'image', maxCount: 10
 }])
