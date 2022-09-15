87c7cb6446565698e7f76b8528a8da295247fdf5
LucianoBrumer <lucianobrumer5@gmail.com>
2022-09-15 16:57:04 -0300

    admin token validation

--- src/routes/location.route.js
+++ src/routes/location.route.js
@@ -5,11 +5,12 @@ const controller = require('../controllers/location.controller')
 
 const parseSchema = require('../middlewares/parseSchema')
 const locationExist = require('../middlewares/locationExist')
+const adminTokenValidation = require('../middlewares/adminTokenValidation')
 
-router.post('/', [parseSchema(Location.UploadSchema)], controller.post)
+router.post('/', [parseSchema(Location.UploadSchema), adminTokenValidation], controller.post)
 router.get('/many/:name', [locationExist], controller.getMany)
 router.get('/:name', controller.get)
-router.put('/:name', [parseSchema(Location.UpdateSchema), locationExist], controller.put)
-router.delete('/:name', [locationExist], controller.delet)
+router.put('/:name', [parseSchema(Location.UpdateSchema), adminTokenValidation, locationExist], controller.put)
+router.delete('/:name', [adminTokenValidation, locationExist], controller.delet)
 
 module.exports = router
